459df53b1353af512ac0e972d6e32b34b3957f57
CJLynch01 <cjlynch@byui.edu>
2024-06-08 22:57:50 -0600

    Styling and assignment completed for week 7.

--- utilities/index.js
+++ utilities/index.js
@@ -66,11 +66,11 @@ Util.buildItemGrid = async function(data){
     grid = '<div id="vehicle-display">'
     grid += '<img id="vehicle-img" src="' + data.inv_image + ' "alt="Image of '+ data.inv_make + ' ' + data.inv_model 
         +' on CSE Motors" />'
-    grid += '<h2>' + data.inv_make + ' ' + data.inv_model + ' Details'
-    grid += '<h3>Price: $' + data.inv_price + '</h3>'
-    grid += '<div><h3>Description: </h3><p id="vehicle-description">' + data.inv_description + '</p></div>'
-    grid += '<div><h3>Color: </h3><p>' + data.inv_color + '</p></div>' 
-    grid += '<div><h3>Miles: </h3><p id="vehicle-miles">' + data.inv_miles + '</p></div>'
+    grid += '<h2 id="vehicle-title">' + data.inv_make + ' ' + data.inv_model + ' Details'
+    grid += '<h3 id="vehicle-price">Price: $' + data.inv_price + '</h3>'
+    grid += '<div id="vehicle-description"><h3>Description: </h3><p>' + data.inv_description + '</p></div>'
+    grid += '<div id="vehicle-color"><h3>Color: </h3><p>' + data.inv_color + '</p></div>' 
+    grid += '<div id="vehicle-miles"><h3>Miles: </h3><p>' + data.inv_miles + '</p></div>'
     grid += '</div>'
   } else {
     grid += '<p class="notice">Sorry, no matching vehicle could be found.</p>'
