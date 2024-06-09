fbb9419af89d40442803f5316e0888231e85ee8e
CJLynch01 <cjlynch@byui.edu>
2024-06-08 20:37:30 -0600

    Save.

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = new express.Router();
+const utilities = require("../controllers/invController");
 const { invCont, errormess } = require("../controllers/invController");
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invCont.buildByClassificationId);
+router.get("/type/:classificationId", utilities.handleErrors(invCont.buildByClassificationId));
 
 // Route to build single view
-router.get("/detail/:inventoryId", invCont.buildByInventoryId);
+router.get("/detail/:inventoryId", utilities.handleErrors(invCont.buildByInventoryId));
 
 // Intentional Error Route
-router.get("/trigger-error", errormess.buildError);
+router.get("/trigger-error", utilities.handleErrors(errormess.buildError));
 
 module.exports = router;
