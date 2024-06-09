b0513dea48201adf3086d2b8956c258331b6a84d
CJLynch01 <cjlynch@byui.edu>
2024-06-08 23:07:29 -0600

    Save. Fix Styling error for W7 Assignment.

--- utilities/index.js
+++ utilities/index.js
@@ -67,10 +67,10 @@ Util.buildItemGrid = async function(data){
     grid += '<img id="vehicle-img" src="' + data.inv_image + ' "alt="Image of '+ data.inv_make + ' ' + data.inv_model 
         +' on CSE Motors" />'
     grid += '<h2 id="vehicle-title">' + data.inv_make + ' ' + data.inv_model + ' Details'
-    grid += '<h3 id="vehicle-price">Price: $' + data.inv_price + '</h3>'
+    grid += '<h3 id="vehicle-price">Price: $' + new Intl.NumberFormat('en-US').format(data.inv_price) + '</h3>'
     grid += '<div id="vehicle-description"><h3>Description: </h3><p>' + data.inv_description + '</p></div>'
     grid += '<div id="vehicle-color"><h3>Color: </h3><p>' + data.inv_color + '</p></div>' 
-    grid += '<div id="vehicle-miles"><h3>Miles: </h3><p>' + data.inv_miles + '</p></div>'
+    grid += '<div id="vehicle-miles"><h3>Miles: </h3><p>' + new Intl.NumberFormat('en-US').format(data.inv_miles) + '</p></div>'
     grid += '</div>'
   } else {
     grid += '<p class="notice">Sorry, no matching vehicle could be found.</p>'
