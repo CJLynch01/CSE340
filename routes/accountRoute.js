13e46d9209b20411d8b9ba83461cf52acbfacd42
CJLynch01 <cjlynch@byui.edu>
2024-06-11 21:55:36 -0600

    Save. No errors.

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = new express.Router();
 const utilities = require("../utilities/index");
 const accountController = require("../controllers/accountController");
+const regValidate = require('../utilities/account-validation')
 
 // Route to build inventory by classification view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
@@ -9,7 +10,12 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 //Route to build registration view
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
-//Route post
-router.post("/register", utilities.handleErrors(accountController.registerAccount))
+//Process registration data
+router.post(
+    "/register", 
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+)
 
 module.exports = router;
